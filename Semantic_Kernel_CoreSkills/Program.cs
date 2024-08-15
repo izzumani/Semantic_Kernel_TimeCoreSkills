6b229caaea384eee303844838306bf0c06ff1323
Isaac Njiru <isaac.njiru@gmail.com>
2024-08-15 12:05:29 -0600

    Add project files.

---
+++ Semantic_Kernel_CoreSkills/Program.cs
@@ -0,0 +1,36 @@
+﻿using Semantic_Kernel_CoreSkills;
+#pragma warning disable SKEXP0050
+using Microsoft.SemanticKernel;
+
+using Microsoft.SemanticKernel.Plugins.Core;
+
+public class Program
+
+{
+    private static Kernel _kernel;
+    public static void Main(string[] args)
+    {
+        var (apiKey, orgId) = Settings.LoadFromSecrets();
+        var builder = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion("gpt-3.5-turbo", apiKey, orgId, serviceId: "gpt35");
+
+        builder.Plugins.AddFromType<TimePlugin>(pluginName: "time");
+
+        _kernel =  builder.Build();
+        InvokeTimePlugin();
+        Console.ReadLine();
+    }
+
+    public static async void InvokeTimePlugin()
+    {
+        const string promptTemplate = @"
+            Today is: {{time.date}}
+            Current time is: {{time.time}}
+            Answer to the following questions using JSON syntax, including the data used.
+            Is it morning, afternoon, evening, or night (morning/afternoon/evening/night)?
+            Is it weekend time (weekend/not weekend)?
+";
+        var results = await _kernel.InvokePromptAsync(promptTemplate);
+        Console.WriteLine(results);
+    }
+}
---
+++ Semantic_Kernel_CoreSkills/Settings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semantic_Kernel_CoreSkills
+{
+    public class Settings
+    {
+        public static (string apiKey, string? orgId) LoadFromSecrets()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                       .AddUserSecrets<Settings>()
+                       .Build();
+
+            try
+            {
+
+                string? apiKey = string.IsNullOrEmpty(config["apiKey"]) ? null : config["apiKey"];
+                string? orgId = config["orgId"] ?? null;
+                return (apiKey, orgId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.Message);
+                return ("", "");
+            }
+        }
+    }
+}
